a1ea6f5d73441609f360b4669c5556a416f35fc2
Zahra Mammadli <zahra.mrj@gmail.com>
2022-02-28 22:18:33 +0100

    Welcome message

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import "./App.css";
-import Backgrdound from "./scripts/background";
-// import { useEffect, useState } from "react";
-// import ParticleBackground from "./scripts/ParticleBackground";
-
+import CenterTitle from "./components/welcome";
+import Backgrdound from "./components/background";
 function App() {
-  // const [showComponent, setShowComponent] = useState(false);
-
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     setShowComponent(!showComponent);
-  //   }, 5000);
-  // }, []);
   return (
     <div>
-      {/* {showComponent ? <Backgrdound /> : <ParticleBackground />} */}
       <Backgrdound />
       <CenterTitle />
-    </div>
-  );
-}
-
-function CenterTitle() {
-  return (
-    <div id="text_div center_all">
-      <div className="center_all">
-        <h1 className="custom-subTitle">Try to Predict the Future</h1>
-      </div>
-      <script src="./scripts/bcg.js"></script>
     </div>
   );
 }
