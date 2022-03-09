b82197a068ca47a60c89301695468dcb601d3bf5
Zahra Mammadli <zahra.mrj@gmail.com>
2022-03-09 21:10:58 +0100

    Create prediction typedefs and resolvers tested in insomnia

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User } = require("../models");
+const { User, Prediction } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
@@ -20,7 +20,45 @@ const resolvers = {
       const token = signToken(user);
       return { user, token };
     },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    //  Todo: replace prediction Author by user. Update both prediction and User
+    addPrediction: async (
+      parent,
+      { predictionText, predictionAuthor, tags }
+    ) => {
+      const prediction = Prediction.create({
+        predictionText,
+        predictionAuthor,
+        tags,
+      });
+
+      await User.findOneAndUpdate(
+        { username: predictionAuthor },
+        { $addToSet: { prediction: prediction._id } }
+      );
+
+      return prediction;
+    },
   },
+
+  //  Todo: implement remove prediction functionality
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,15 +13,40 @@ const typeDefs = gql`
     username: String
     email: String
     password: String
+    predictions: [Prediction]
   }
+
   type Auth {
     token: ID!
     user: User
   }
 
+  type Prediction {
+    _id: ID
+    predictionText: String
+    predictionAuthor: String
+    createdAt: String
+    predictionDate: String
+    tags: String
+    comments: [Comment]
+  }
+
+  type Comment {
+    _id: ID
+    commentText: String
+    commentAuthor: String
+    createdAt: String
+  }
+
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+    addPrediction(
+      predictionText: String!
+      predictionAuthor: String!
+      tags: String
+    ): Prediction
+    removePrediction(predictionId: String!): Prediction
   }
 `;
 
