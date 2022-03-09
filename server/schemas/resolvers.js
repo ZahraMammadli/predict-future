7c57bcd57a2485a8d9fc09a4eb73e9c3135e05dd
Zahra Mammadli <zahra.mrj@gmail.com>
2022-03-09 20:16:53 +0100

    Create User and Login typedefs and resolvers added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,24 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User, Thought } = require("../models");
+const { User } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     msg: () => {
       return "hello world";
+    },
+    users: async () => {
+      return User.find();
+    },
+    user: async (parent, args) => {
+      return User.find(args.username);
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { user, token };
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,27 @@
 const { gql } = require("apollo-server-express");
+const { User } = require("../models");
 
 const typeDefs = gql`
   type Query {
     msg: String
+    users: [User]
+    user(username: String!): User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
   }
 `;
 
