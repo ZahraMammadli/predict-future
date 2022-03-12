e32efa94c595c13a251ae3520e73c8d05a33affd
alexanderpuschkinberlin <90645435+alexanderpuschkinberlin@users.noreply.github.com>
2022-03-12 14:39:03 +0100

    add trend section

--- client/src/components/RightTab/RightTab.js
+++ client/src/components/RightTab/RightTab.js
@@ -1,12 +1,22 @@
 import SearchBar from "../SearchBar/SearchBar";
 import "./RightTab.css";
+import Trend from "./Trend";
+
+const trends = [
+  { name: "#Trend1" },
+  { name: "#Trend2" },
+  { name: "#Trend3" },
+  { name: "#Trend4" },
+];
 
 export default function RightTab() {
   return (
     <div className="right-tab">
       <SearchBar />
       <div className="">
-        <h3>Side bar</h3>
+        {trends.map((t) => (
+          <Trend trend={t.name} />
+        ))}
       </div>
     </div>
   );
