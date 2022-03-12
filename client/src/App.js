3cc09cc94fde5b9ad2a8d6b7b6a83842e8c079be
Zahra Mammadli <zahra.mrj@gmail.com>
2022-03-12 11:56:35 +0100

    Feed UI initial version

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import CenterTitle from "./components/welcome";
 import Backgrdound from "./components/background";
-import Stack from "@mui/material/Stack";
 import Predictions from "./components/PredictionStream/predictions";
 
 function App() {
@@ -10,20 +9,18 @@ function App() {
       {/* <Backgrdound /> */}
       {/* <CenterTitle /> */}
       <div className="Predictions">
-        <Stack spacing={1}>
-          <Predictions
-            User="Zahra Mammadli"
-            Text="I predict cars will fly by 2024"
-          />
-          <Predictions
-            User="Homer Simpson"
-            Text="Swapnil will be prime minister of UK"
-          />
-          <Predictions
-            User="Test User 5"
-            Text="The war will be over by first week of April 2022"
-          />
-        </Stack>
+        <Predictions
+          User="Zahra Mammadli"
+          Text="I predict cars will fly by 2024"
+        />
+        <Predictions
+          User="Homer Simpson"
+          Text="Swapnil will be prime minister of UK"
+        />
+        <Predictions
+          User="Test User 5"
+          Text="The war will be over by first week of April 2022"
+        />
       </div>
     </div>
   );
--- client/src/components/PredictionStream/predictions.js
+++ client/src/components/PredictionStream/predictions.js
@@ -1,43 +1,55 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
-import "./post.css";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import RepeatIcon from "@material-ui/icons/Repeat";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import Grid from "@material-ui/core/Grid";
 
 export default function Predictions(props) {
   return (
-    <Card
-      sx={{
-        maxWidth: 345,
-        flexWrap: "wrap",
-        justifyContent: "center",
-        alignContent: "center",
-        padding: "3px",
-      }}
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: "13vh" }}
     >
-      {/* <CardMedia
+      <Grid item xs={10}>
+        <Card
+          sx={{
+            maxWidth: 555,
+            minWidth: 555,
+            justifyContent: "center",
+            alignContent: "center",
+            padding: "3px",
+          }}
+        >
+          {/*TODO:  We can use this part later on for gif API */}
+          {/* <CardMedia
         component="img"
         height="140"
         image="/static/images/cards/contemplative-reptile.jpg"
         alt="green iguana"
       /> */}
 
-      <CardContent>
-        <span className="post__headerSpecial">{props.User}</span>
-        <div className="post__headerDescription">
-          <p>{props.Text}</p>
-        </div>
-      </CardContent>
-      <CardActions>
-        <div className="post__footer">
-          <ChatBubbleOutlineIcon fontSize="small" />
-          <RepeatIcon fontSize="small" />
-          <FavoriteBorderIcon fontSize="small" />
-        </div>
-      </CardActions>
-    </Card>
+          <CardContent>
+            <span className="post__headerSpecial">{props.User}</span>
+            <div className="post__headerDescription">
+              <p>{props.Text}</p>
+            </div>
+          </CardContent>
+          <CardActions>
+            <div className="post__footer">
+              <ChatBubbleOutlineIcon fontSize="small" />
+              <RepeatIcon fontSize="small" />
+              <FavoriteBorderIcon fontSize="small" />
+            </div>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
