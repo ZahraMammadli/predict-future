9f09a618bbd7c97a7bcd5d27b35312a3007464f6
Zahra Mammadli <zahra.mrj@gmail.com>
2022-03-14 19:10:16 +0100

    Predictions feed functioning and linked to the back end

--- client/src/App.js
+++ client/src/App.js
@@ -41,23 +41,9 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <div>
-        {/* <Backgrdound /> */}
-        {/* <CenterTitle /> */}
-        <div className="Predictions">
-          <Feed />
-          {/* <Predictions
-            User="Zahra Mammadli"
-            Text="I predict cars will fly by 2024"
-          />
-          <Predictions
-            User="Homer Simpson"
-            Text="Swapnil will be prime minister of UK"
-          />
-          <Predictions
-            User="Test User 5"
-            Text="The war will be over by first week of April 2022"
-          /> */}
-        </div>
+        {/* <Backgrdound />
+        <CenterTitle /> */}
+        <Feed />
       </div>
     </ApolloProvider>
   );
